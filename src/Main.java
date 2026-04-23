e396298ed9f1f114290d3ee51c455e131bd0462d
joe <joe1234550@gmail.com>
2026-04-23 21:25:49 +0800

    feat: 新增遊戲主程式入口

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== VTuber Card Battle ===");
+        System.out.println("遊戲載入中...");
+    }
+}
